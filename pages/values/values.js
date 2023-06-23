2cb6a2214d6f9345d2faacbb64c15dc3e0d533ad
Mario Vinicius <111910275+MarioViniciux@users.noreply.github.com>
2023-06-23 18:45:35 -0300

    Update values.js

--- pages/values/values.js
+++ pages/values/values.js
@@ -10,7 +10,7 @@ function pValueMadeMonth() {
 }
 
 function initialPage() {
-    window.location.href = '../../../initialPage.html'
+    window.location.href = '../../initialPage.html'
 }
 
 // Repassa o valor da quantidade vendida mensalmente
